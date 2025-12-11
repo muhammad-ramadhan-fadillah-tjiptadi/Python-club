72c1a906e1091b6609aecb8310ac70990b4495db
Byramadhan <rmafdllhcptd@gmail.com>
2025-12-11 17:10:13 +0700

    add: templating

--- Flask-Project/app.py
+++ Flask-Project/app.py
@@ -1,23 +1,43 @@
-from os import name
-from flask import Flask,render_template
+from flask import Flask, redirect,render_template, request, session, url_for
 
 app = Flask(__name__)
+app.secret_key = "secret123"
+
+USER = {
+    "username": "admin",
+    "password": "1234"
+}
 
 @app.route('/')
-def index():
-    return render_template("index.html")
+def home():
+    return redirect(url_for('login'))
 
-@app.route('/hello')
-def hello():
-    return "Hello World"
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        if username == USER['username'] and password == USER['password']:
+            session['logged_in'] = True
+            session['username'] = username
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('login.html', error='Username atau Password Salah')
 
-@app.route('/profile/<username>')
-def show_profile(username):
-    return render_template("profile.html", username=username)
+    return render_template('login.html')
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    return 'Post ID: %d' % post_id
+@app.route('/dashboard')
+def dashboard():
+    if not session.get('logged_in'):
+        return redirect(url_for('login'))
 
-if __name__ == '__main__':
+    return render_template('dashboard.html', user=session.get('username'))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+if __name__ == "__main__":
     app.run(debug=True)
